add45d00d0e80a202159ea8d920ebe3f03c42090
AlexJimenez-ITB <alex.jimenez.7e6@itb.cat>
2024-09-26 10:02:48 +0200

    Prueba

---
+++ ComprovarEdat.py
@@ -0,0 +1,8 @@
+# Definir edat com a sencer
+edat = int(input("Digues-me la teva edat: "))  # Llegir edat
+
+# Comprovar si edat és major o igual a 18
+if edat >= 18:  
+    print("Ets major d'edat")  # Escriure si és major d'edat
+
+print("Programa acabat")  # Escriure el final del programa
